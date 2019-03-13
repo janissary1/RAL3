82863a8e3e3981ae3daecf68ea424db21f58d8e2
Tony Misic <tony.misic98@gmail.com>
2019-03-13 00:18:45 -0400

    sleep

--- mc.cpp
+++ mc.cpp
@@ -14,18 +14,37 @@
 #include <chrono>
 #include <algorithm>
 #include <numeric>
+//prints cpu time
+void printRun(std::chrono::duration<double> run_time) {
+    std::cout << "Computation Time: " << run_time.count() << "s" << std::endl;
+}
+double prob1, prob2;
 
-int main(int argc, char** args){
+int main(int argc, char** argv) {
+    
+    srand(time(NULL));
+    if ( argc > 1 ) {
+        prob1 = atof(argv[1]);
+        prob2 = atof(argv[2]);
+    } else {
+        std::cout << "Not enough arguments!" << std::endl;
+    }
+    auto start = std::chrono::system_clock::now(); // start time
+    // ----------------- monte carlo learning ----------------- //
     double policy[4] = {0.25,0.25,0.25,0.25};
     char** board = build_board();
-    std::vector<std::array<int,4>> t = generateEpisode(policy,board);
-    
+    std::vector<std::array<int,4>> episode = generateEpisode(policy,board);
     for(int i = 0; i < 10; i++){
         for(int k = 0; k < 10; k++){
-            std::cout << board[k][i];
+            printf("%2.c",board[k][i]);
         }
         std::cout << "\n";
     }
+    // ----------------- monte carlo learning ----------------- //
+    auto end = std::chrono::system_clock::now(); // end time
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "Total ";
+    printRun(elapsed_seconds);
    
     return 0;
 }
