da48ec78169284fa34f2118cb4da8d6f69152a03
janissary1 <bellissimo38@gmail.com>
2019-03-13 19:19:47 -0400

    Refactored episode generation

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 
 int main(int argc, char** args){
     double policy[4] = {0.25,0.25,0.25,0.25};
-    char** board = build_board();
-    generateEpisode(policy,board);
-    
+    std::array<std::array<char,10>,10> board = build_board();
+    std::vector< std::array<int,4> > out = generateEpisode(policy,1.0,0.0);
+    for(int i = 0 ; i < 20; i++){
+        std::printf("%d,%d:%d,%d\n",out[i][0],out[i][1],out[i][2],out[i][3]);
+    }
+    /*
     for(int i = 0; i < 10; i++){
         for(int k = 0; k < 10; k++){
             std::cout << board[k][i];
         }
         std::cout << "\n";
-    }
+    }*/
    
     return 0;
+    
 }
