96b3c74bcee91356bd36d08444ce766282886e56
Tony Misic <tony.misic98@gmail.com>
2019-03-13 00:50:25 -0400

    shleep

--- mc.cpp
+++ mc.cpp
@@ -34,9 +34,9 @@ int main(int argc, char** argv) {
     double policy[4] = {0.25,0.25,0.25,0.25};
     char** board = build_board();
     std::vector<std::array<int,4>> episode = generateEpisode(policy,board);
-    for(int i = 0; i < 10; i++){
-        for(int k = 0; k < 10; k++){
-            printf("%2.c",board[k][i]);
+    for(int i = 0; i < 10; i++) {
+        for(int k = 0; k < 10; k++) {
+            printf("%2.c", board[k][i]);
         }
         std::cout << "\n";
     }
